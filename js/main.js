72567dde241c682da173577995e242767a233f86
Agustín Gaviña <agusgavina2@hotmail.com>
2023-05-25 23:01:22 -0300

    Tabla dinamica funcional en el html producto

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const MAX = 10000;
 const MIN = 1000;
@@ -11,30 +11,32 @@ let segurityCode;
 form.addEventListener("submit", validar);
 codigoSeguridad();
 
-function codigoSeguridad(){
-    segurityCode = Math.floor((Math.random()*(MAX - MIN)+MIN));
-    captcha.innerHTML = segurityCode; 
+function codigoSeguridad() {
+  segurityCode = Math.floor(Math.random() * (MAX - MIN) + MIN);
+  captcha.innerHTML = segurityCode;
 }
 
-function validar(e){
-    e.preventDefault();
-    let inputClave = document.querySelector("#clave");
-    let formData = new FormData(form);
+function validar(e) {
+  e.preventDefault();
+  let inputClave = document.querySelector("#clave");
+  let formData = new FormData(form);
 
-    let nombre = formData.get('nombre');
-    let email = formData.get('email');
-    let clave = formData.get('validacion');
+  let nombre = formData.get("nombre");
+  let email = formData.get("email");
+  let clave = formData.get("validacion");
 
-    if(clave != segurityCode){
-        mensaje.innerHTML = "Ha ingresado incorrectamente el codigo de seguridad. Pruebe de vuelta.";
-        mensaje.classList.add("colorRED");
-        codigoSeguridad();
-        inputClave.value = ""
-    }
-    else{
-        mensaje.innerHTML = "Sus datos se enviaron correctamente";
-        mensaje.classList.remove("colorRED");
-        codigoSeguridad();
-        inputClave.value = ""
-    }
+  if (clave != segurityCode) {
+    mensaje.innerHTML =
+      "Ha ingresado incorrectamente el codigo de seguridad. Pruebe de vuelta.";
+    mensaje.classList.add("colorRED");
+    codigoSeguridad();
+    inputClave.value = "";
+  } else {
+    mensaje.innerHTML = "Sus datos se enviaron correctamente";
+    mensaje.classList.remove("colorRED");
+    codigoSeguridad();
+    inputClave.value = "";
+  }
 }
+
+
